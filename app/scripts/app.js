8165d960c3d92b0ebb57d7c8d27517b4146139ec
Dylan D <dotolo.dylan2@gmail.com>
2015-11-19 19:45:54 -0600

    project pages added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -44,6 +44,47 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      //PROJECT VIEWS START HERE------------------///////////////
+      .when('/txpoolspa', {
+        templateUrl: 'views/project-views/txpoolspa.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/rawcomplextion', {
+        templateUrl: 'views/project-views/rawcomplextion.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/travelers', {
+        templateUrl: 'views/project-views/world-travelers.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/student-port', {
+        templateUrl: 'views/project-views/student-port.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/photography', {
+        templateUrl: 'views/project-views/photography.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/personal-trainer', {
+        templateUrl: 'views/project-views/personal-trainer.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/wedding', {
+        templateUrl: 'views/project-views/wedding.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/mothers-day', {
+        templateUrl: 'views/project-views/mothers-day.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
       .otherwise({
         redirectTo: '/'
       });
